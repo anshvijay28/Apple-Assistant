bd77bb2a0e2d66396dcefbb0a9bea8684050e6a4
Ansh Vijay <anshvijay28@gmail.com>
2023-01-19 12:12:56 -0500

    uncommenting get_body

--- construct_message.py
+++ construct_message.py
@@ -3,20 +3,16 @@ import temperature
 def get_subject():
 	subject = str(temperature.CITY) + ": " + "It's " + str(temperature.current_date) + "!"
 	return subject
-# def get_body():
-# 	body = '''
-
-# 	It currently feels {feels_like}°F.
-# 	The real temperature is {actual}°F.
-# 	The low today will be {low}°F.
-# 	The high today will be {high}°F.
-# 	The sun will set at {sunset}.
-# 	'''.format(feels_like = str(temperature.feels_like), actual = str(temperature.actual),
-# 		low = str(temperature.low), high = str(temperature.high), 
-# 		sunset = str(temperature.time_of_sunset))
-# 	return body
-
 def get_body():
 	body = '''
+
+	It currently feels {feels_like}°F.
+	The real temperature is {actual}°F.
+	The low today will be {low}°F.
+	The high today will be {high}°F.
 	The sun will set at {sunset}.
-	'''.format(sunset = str(temperature.time_of_sunset))
+	'''.format(feels_like = str(temperature.feels_like), actual = str(temperature.actual),
+		low = str(temperature.low), high = str(temperature.high), 
+		sunset = str(temperature.time_of_sunset))
+	return body
+
